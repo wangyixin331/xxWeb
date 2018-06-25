a500eb1e8f7765d12dccc10b036da46174e12148
Will Wang <freedomisours@126.com>
2018-06-25 21:27:01 +0800

    Initial commit

---
+++ first/School/src/main/java/com/web/personOne.java
@@ -0,0 +1,48 @@
+package com.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class personOne
+ */
+public class personOne extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public personOne() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("<html>\n" + 
+				"<body>\n" + 
+				"<h2>Hello World!</h2>\n" + 
+				"</body>\n" + 
+				"</html>\n" + 
+				"");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
